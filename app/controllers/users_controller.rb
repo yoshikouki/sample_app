4e23f7f1935357c793214bb56ec9187925360920
yoshiko <yoshikouki@gmail.com>
2020-01-28 23:42:05 +0000

    Implement basic login. 第8章修了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,21 @@
 class UsersController < ApplicationController
   def new
   end
+  
   def show
     @user = User.find( params[:id] )
   end
+  
   def new
     @user = User.new
   end
+  
   def create
     @user = User.new(user_params)
     if @user.save
+      # 作成したユーザーでログイン
+      log_in @user
+      # ユーザー登録が完了したメッセージを作成
       flash[:success] = "Welcome to the Sample App!"
       redirect_to user_url(@user)
     else
@@ -18,6 +24,7 @@ class UsersController < ApplicationController
   end
   
   private
+    # StrongParam
     def user_params
       params.require(:user).permit(:name,:email,:password,:password_confirmation)
     end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,4 +11,8 @@ class ActiveSupport::TestCase
   # Add more helper methods to be used by all tests here...
   include ApplicationHelper
   
+  # テストユーザーがログイン中の場合にtrue
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
