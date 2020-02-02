61418291706efdc948e83ebb65a550b1bfd87f25
yoshiko <yoshikouki@gmail.com>
2020-02-02 15:24:06 +0000

    Add user destroy actions. Finish Chapter 10. 第10章 修了

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   
-  before_action :logged_in_user,  only: [:edit, :update, :index]
+  before_action :logged_in_user,  only: [:edit, :update, :index, :destroy]
   before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: [:destroy]
   
   def index
     @users = User.paginate(page: params[:page])
@@ -47,11 +48,18 @@ class UsersController < ApplicationController
     end
   end
   
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_path
+  end
+  
   private
   
     # マスアサインメントの脆弱性対策（Strong Parameters）
     def user_params
-      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+      params.require(:user).permit(:name,:email,:password,
+                                   :password_confirmation)
     end
     
     # beforeアクション
@@ -69,5 +77,10 @@ class UsersController < ApplicationController
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    # 管理者かどうかの確認
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
 end
