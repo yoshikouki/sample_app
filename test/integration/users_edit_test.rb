f2e5e226924f77eb49206684f5b9009e63050573
yoshiko <yoshikouki@gmail.com>
2020-02-02 14:29:14 +0000

    add users pagination. 第10章 3節 修了

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -4,7 +4,6 @@ class UsersEditTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
-    @other_user = users(:archer)
   end
 
   test "unsuccessful edit" do
@@ -21,10 +20,12 @@ class UsersEditTest < ActionDispatch::IntegrationTest
   
   test 'successful edit with friendly forwarding' do
     get edit_user_path(@user)
+    # フレンドリーフォワーディングのテスト
     assert_equal edit_user_url(@user), session[:forwarding_url]
     log_in_as(@user)
     assert_redirected_to edit_user_url(@user)
     assert session[:forwarding_url].nil?
+    
     name  = "Foo Bar"
     email = "foo@bar.com"
     patch user_path(@user), params: { user: { name:  name,
@@ -36,33 +37,5 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     @user.reload
     assert_equal name,  @user.name
     assert_equal email, @user.email
-  end
-  
-  test "should redirect edit when not logged in" do
-    get edit_user_path(@user)
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-  
-  test "should redirect update when not logged in" do
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert_not flash.empty?
-    assert_redirected_to login_url
-  end
-  
-  test "should redirect update when logged in as wrong user" do
-    log_in_as(@other_user)
-    get edit_user_path(@user)
-    assert flash.empty?
-    assert_redirected_to root_url
-  end
-  
-  test "should redirect edit when logged in as wrong user" do
-    log_in_as(@other_user)
-    patch user_path(@user), params: { user: { name: @user.name,
-                                              email: @user.email } }
-    assert flash.empty?
-    assert_redirected_to root_url
   end
 end
