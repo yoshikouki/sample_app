5678b2cda00189eae5dfbadea5325866dc3f3dfc
yoshiko <yoshikouki@gmail.com>
2020-02-12 23:11:06 +0000

    Add feed test. 演習14.3.3.1

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -53,4 +53,12 @@ class FollowingTest < ActionDispatch::IntegrationTest
       delete relationship_path(relationship), xhr: true
     end
   end
+  
+  # フィードのHTMLのテスト
+  test "feed on Home page" do
+    get root_path
+    @user.feed.paginate(page: 1).each do |micropost|
+      assert_match CGI.escapeHTML(micropost.content), response.body
+    end
+  end
 end
